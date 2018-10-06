e8dffc1a3911de252fe2a5fc3871bb87a58d0810
MartinNoy <MartinNoy98@hotmail.com>
2018-10-06 12:35:34 -0300

    agregado de entrada

--- Tp-TicketUNLa/src/ticketUNLa/Entrada.java
+++ Tp-TicketUNLa/src/ticketUNLa/Entrada.java
@@ -1,5 +1,45 @@
 package ticketUNLa;
 
 public class Entrada {
+	
+	private int numeroEntrada;
+	private double valorfinalEntrada;
+	private String ubicacionSeleccionada;
+	
+	public Entrada(int numeroEntrada, double valorfinalEntrada, String ubicacionSeleccionada) {
+		this.numeroEntrada = numeroEntrada;
+		this.valorfinalEntrada = valorfinalEntrada;
+		this.ubicacionSeleccionada = ubicacionSeleccionada;
+	}
 
+	public int getNumeroEntrada() {
+		return numeroEntrada;
+	}
+
+	public void setNumeroEntrada(int numeroEntrada) {
+		this.numeroEntrada = numeroEntrada;
+	}
+
+	public double getValorfinalEntrada() {
+		return valorfinalEntrada;
+	}
+
+	public void setValorfinalEntrada(double valorfinalEntrada) {
+		this.valorfinalEntrada = valorfinalEntrada;
+	}
+
+	public String getUbicacionSeleccionada() {
+		return ubicacionSeleccionada;
+	}
+
+	public void setUbicacionSeleccionada(String ubicacionSeleccionada) {
+		this.ubicacionSeleccionada = ubicacionSeleccionada;
+	}
+
+	
+	
+public String imprimirTicket() {//agregar cantidad de tikets comprada por un usuario y usarlo para imprimir distintas partes de la lista
+	
+	
+}
 }
