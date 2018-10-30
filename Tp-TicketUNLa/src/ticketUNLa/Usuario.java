03124a7377a7a44124eb82f67453b2b3d1244449
MartinNoy <martinnoy98@hotmail.com>
2018-10-30 11:19:03 -0300

    calcular edad modificado

--- Tp-TicketUNLa/src/ticketUNLa/Usuario.java
+++ Tp-TicketUNLa/src/ticketUNLa/Usuario.java
@@ -2,6 +2,7 @@ package ticketUNLa;
 import java.util.*;
 
 
+
 public class Usuario {
 	protected long id;
 	protected int dni;
@@ -49,15 +50,30 @@ public class Usuario {
 	}
 
 	
-public long calcularEdad() {
-	GregorianCalendar FechaActual = new GregorianCalendar();
-	long edad;
-	edad=FechaActual.getTimeInMillis()-nacimiento.getTimeInMillis();
-	edad=edad/(1000*60*60*24);
-	return edad/365;
-	
-	
-}
+	public int calcularEdad() {
+		GregorianCalendar FechaActual = new GregorianCalendar();
+		
+		
+		/*long edad;
+		edad=FechaActual.getTimeInMillis()-nacimiento.getTimeInMillis();
+		edad=edad/(1000*60*60*24);
+		return edad/365;*/
+		
+		
+		int diffYear = FechaActual.get(Calendar.YEAR) - nacimiento.get(Calendar.YEAR);
+	    int diffMonth = FechaActual.get(Calendar.MONTH) - nacimiento.get(Calendar.MONTH);
+	    int diffDay = FechaActual.get(Calendar.DAY_OF_MONTH) - nacimiento.get(Calendar.DAY_OF_MONTH);
+	    
+	    // Si está en ese año pero todavía no los ha cumplido
+	    
+	    if (diffMonth < 0 || (diffMonth == 0 && diffDay < 0)) {
+	        diffYear = diffYear - 1;
+	    }
+	    
+	    return diffYear;
+		
+		
+	}
 
 @Override
 public String toString() {
