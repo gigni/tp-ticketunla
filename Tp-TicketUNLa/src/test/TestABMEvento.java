96a2d409660916f4feb2a49a6f5638abe5ba5f40
waltertmed <waltertmedina@gmail.com>
2018-11-06 23:19:13 -0300

    ABMS terminados

--- Tp-TicketUNLa/src/test/TestABMEvento.java
+++ Tp-TicketUNLa/src/test/TestABMEvento.java
@@ -10,6 +10,9 @@ public class TestABMEvento {
 
 	public static void main(String[] args) {
 		try {
+			System.out.println("##################################### Agrego un Evento #######################################");
+			System.out.println("##################################### Agrego un Evento #######################################");
+			System.out.println("##################################### Agrego un Evento #######################################");
 			SistemaTicketUNLA e = new SistemaTicketUNLA();
 			
 			e.agregarEvento("Juanito y los Clonosaurios",0.05 , 0.15);
@@ -39,11 +42,93 @@ public class TestABMEvento {
 			evento3.agregarFuncion(f1, auditorio3, 0.25);
 			evento3.agregarTarifa(400, auditorio3.traerSector("plate"));
 			System.out.println(e.imprimirEventos());
-			
 		}
-		catch(Exception e){
+		catch(Exception e) {
 			System.out.println("Excepcion:"+ e.getMessage());
 		}
+			
+		try {
+			System.out.println("##################################### Modifico un Evento #######################################");
+			SistemaTicketUNLA e = new SistemaTicketUNLA();
+			
+			e.agregarEvento("Juanito y los Clonosaurios",0.05 , 0.15);
+			e.agregarEvento("La vela puerca", 0.30, 0.50);
+			
+			e.agregarAuditorio("unla", "cine", "calle falsa 123");
+			e.agregarAuditorio("lavalle", "cine", "lavalle 780");
 
+			Auditorio auditorio=e.traerAuditorio(1);
+			Auditorio auditorio2=e.traerAuditorio(2);
+			auditorio.agregarSector("pullman", 5, 2, 2, 1);
+			auditorio.agregarSector("palco", 10, 2, 3, 4);
+			auditorio2.agregarSector("popular", 5, 0, 0, 5);
+			auditorio2.agregarSector("platea", 15, 3, 5, 0);
+
+			GregorianCalendar f1 = new GregorianCalendar(2018,11,19,20,30,00);	
+			GregorianCalendar f2 = new GregorianCalendar(2018,10,30,17,00,00);
+			
+			Evento evento = e.traerEvento(1);
+			evento.agregarFuncion(f1,auditorio, 0.05);
+			evento.agregarTarifa(230,auditorio.traerSector("pullman"));
+			e.modificarEvento(1, "Julito Y los Clonosaurios");
+			
+			
+			Evento evento2 = e.traerEvento(2);
+			evento2.agregarFuncion(f2, auditorio2, 0.09);
+			evento2.agregarTarifa(450, auditorio2.traerSector("popular"));
+			e.modificarEvento(2, "La Lampara puerca");
+			
+			
+			System.out.println(e.imprimirEventos());
+		}
+		catch(Exception e) {
+			System.out.println("Excepcion:"+ e.getMessage());
+		}
+	try {
+		System.out.println("##################################### Elimino un Evento #######################################");
+		SistemaTicketUNLA e = new SistemaTicketUNLA();
+		
+		e.agregarEvento("Juanito y los Clonosaurios",0.05 , 0.15);
+		e.agregarEvento("La vela puerca", 0.30, 0.50);
+		
+		e.agregarAuditorio("unla", "cine", "calle falsa 123");
+		e.agregarAuditorio("lavalle", "cine", "lavalle 780");
+
+		Auditorio auditorio=e.traerAuditorio(1);
+		Auditorio auditorio2=e.traerAuditorio(2);
+		auditorio.agregarSector("pullman", 5, 2, 2, 1);
+		auditorio.agregarSector("palco", 10, 2, 3, 4);
+		auditorio2.agregarSector("popular", 5, 0, 0, 5);
+		auditorio2.agregarSector("platea", 15, 3, 5, 0);
+
+		GregorianCalendar f1 = new GregorianCalendar(2018,11,19,20,30,00);	
+		GregorianCalendar f2 = new GregorianCalendar(2018,10,30,17,00,00);
+		
+		Evento evento = e.traerEvento(1);
+		evento.agregarFuncion(f1,auditorio, 0.05);
+		evento.agregarTarifa(230,auditorio.traerSector("pullman"));
+		Evento evento2 = e.traerEvento(2);
+		evento2.agregarFuncion(f2, auditorio2, 0.09);
+		evento2.agregarTarifa(450, auditorio2.traerSector("popular"));
+		
+		e.eliminarEvento(1);
+		System.out.println(e.imprimirEventos());
+		System.out.println("##################################### Lo vuelvo a agregar #######################################");
+		System.out.println("##################################### Lo vuelvo a agregar #######################################");
+		e.agregarEvento("Juanito y los Clonosaurios",0.05 , 0.15);
+		Evento evento3 = e.traerEvento(3);
+		evento3.agregarFuncion(f1,auditorio, 0.05);
+		evento3.agregarTarifa(230,auditorio.traerSector("pullman"));
+		System.out.println(e.imprimirEventos());
+		
+		
 	}
+	catch(Exception e) {
+		System.out.println("Excepcion:" + e.getMessage());
+	}
+			
+
+		}
+		
+	
 }
